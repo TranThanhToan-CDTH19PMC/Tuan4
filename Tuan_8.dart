f854728ade089b30ef5ea65f6e87e6cf20a0c6ac
TranThanhToan-CDTH19PMC <83933063+TranThanhToan-CDTH19PMC@users.noreply.github.com>
2021-11-13 22:36:57 +0700

    Update Tuan_8.dart

--- Tuan_8.dart
+++ Tuan_8.dart
@@ -545,4 +545,568 @@ class Addfiend extends StatelessWidget {
     );
   }
 }
+class FbCloneProfileStful extends StatefulWidget {
+  _FbCloneProfileState createState() => _FbCloneProfileState();
+}
 
+class _FbCloneProfileState extends State<FbCloneProfileStful> {
+  @override
+  Widget build(BuildContext cx) {
+    return new Scaffold(
+      appBar: null,
+      body: new ListView(
+        children: <Widget>[
+          new Column(
+            children: <Widget>[
+              Container(
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  overflow: Overflow.visible,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            height: 200.0,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage('../images/cat.jpg'))),
+                          ),
+                        )
+                      ],
+                    ),
+                    Positioned(
+                      top: 100.0,
+                      child: Container(
+                        height: 190.0,
+                        width: 190.0,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('../images/cat.jpg'),
+                            ),
+                            border:
+                                Border.all(color: Colors.white, width: 6.0)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                height: 130.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Thanh Toàn',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 28.0),
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    Icon(
+                      Icons.check_circle,
+                      color: Colors.blueAccent,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 12.0,
+              ),
+              Container(
+                  child: Text(
+                'Signbox software',
+                style: TextStyle(fontSize: 18.0),
+              )),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.collections, color: Colors.blueAccent),
+                        Text(
+                          'following',
+                          style: TextStyle(color: Colors.blueAccent),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.message, color: Colors.black),
+                        Text(
+                          'Message',
+                          style: TextStyle(color: Colors.black),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.more_vert, color: Colors.black),
+                          onPressed: () {
+                            _showMoreOption(cx);
+                          },
+                        ),
+                        Text(
+                          'More',
+                          style: TextStyle(color: Colors.black),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'Founder and CEO at',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'SignBox',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'Works at',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'SignBox',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.school),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'Studied Computer Science at',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        Text(
+                          'Abc University',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.home),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'Lives in',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'Pakistan',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.location_on),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'From',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'Lahore',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.list),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'Followed by',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          '100K people',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text('see more about Mohsin'),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 10.0,
+                      child: Divider(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Photos',
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Card(
+                                child: Image.asset('../images/xebus.jpg'),
+                              )),
+                              Expanded(
+                                  child: Card(
+                                child: Image.asset('../images/xebus.jpg'),
+                              ))
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                  child: Card(
+                                child: Image.asset('../images/xebus.jpg'),
+                              )),
+                              Expanded(
+                                  child: Card(
+                                child: Image.asset('../images/xebus.jpg'),
+                              )),
+                              Expanded(
+                                  child: Card(
+                                child: Image.asset('../images/xebus.jpg'),
+                              ))
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  _showMoreOption(cx) {
+    showModalBottomSheet(
+      context: cx,
+      builder: (BuildContext bcx) {
+        return new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.feedback,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'Give feedback or report this profile',
+                    style: TextStyle(fontSize: 18.0),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.block,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'Block',
+                    style: TextStyle(fontSize: 18.0),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.link,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'Copy link to profile',
+                    style: TextStyle(fontSize: 18.0),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'Search Profile',
+                    style: TextStyle(fontSize: 18.0),
+                  )
+                ],
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+// thông báo
+class notification extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Thông Báo",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(0),
+        children: [
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat.jpg'),
+            ),
+            title: Text(
+              "Wibu đã đăng 1 tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "30 phút trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat2.jpg'),
+            ),
+            title: Text(
+              "Mi đã đăng một tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "2 tiếng trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat3.jpg'),
+            ),
+            title: Text(
+              "Tàu hủ đã đăng một tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "1 phút trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat4.jpg'),
+            ),
+            title: Text(
+              "Lisa đã đăng một tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "7 phút trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat5.jpg'),
+            ),
+            title: Text(
+              "Kevin đã đăng một tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "3 phút trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat6.jpg'),
+            ),
+            title: Text(
+              "Mimi đã đăng một tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "5 tiếng trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat7.jpg'),
+            ),
+            title: Text(
+              "anna đã đăng một tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "3 tiếng trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            selected: true,
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('../images/cat8.jpg'),
+            ),
+            title: Text(
+              "Elizabeth đã đăng một tin",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: Text(
+              "1 phút trước",
+              style: TextStyle(color: Colors.grey),
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
